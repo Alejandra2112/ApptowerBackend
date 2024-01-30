54ea037fe2f91b09197d7753d34bc79301b4f52f
Emmanuel tabares <emanuel_tabares@hotmail.com>
2024-01-30 12:16:44 -0500

    Update residents controller

--- src/Models/server.js
+++ src/Models/server.js
@@ -136,7 +136,7 @@ class Servers {
 
     try {
 
-      await sequelize.sync({ force: true }).then(() => {
+      await sequelize.sync({ force: false }).then(() => {
         console.log('Models synchronized with the database');
 
       });
