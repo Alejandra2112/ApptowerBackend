157aef981c0cd12bf030ae4492cac6cf3906ce8c
Daniel Rivera <dayorito@gmail.com>
2024-01-30 16:01:01 -0500

    Se creo una funcion para guardar varias imagenes en el post de multas y cambio el modelo de fines

--- src/Controllers/fines.controller.js
+++ src/Controllers/fines.controller.js
@@ -48,7 +48,6 @@ const getFinesOne = async (req, res = response) => {
             error: 'Error al obtener multa',
         });
     }
-
 }
 
 const getFinesByApartment = async (req, res = response) => {
@@ -79,11 +78,12 @@ const postFines = async (req, res) => {
     
 
     try {
-        const imageUrl = await upload(req.files.evidenceFiles, ['pdf','jpg','jpeg','png'], 'Evidences')
-        console.log(imageUrl);
+        const images = req.files.evidenceFiles;
+        const imagesUrl = await Promise.all(images.map(async(file)  => await upload(file, ['pdf','jpg','jpeg','png'], 'Evidences')))
+        console.log(imagesUrl);
         const {evidenceFiles, ...finesAtributes } = req.body;
         await Fines.create({
-            evidenceFiles: imageUrl,
+            evidenceFiles: imagesUrl,
             ...finesAtributes,
         });
         message = 'Multa Registrada Exitosamente';
@@ -95,62 +95,108 @@ const postFines = async (req, res) => {
     });
 };
 
-const putFines = async (req, res = response) => {
-    const body = req.body;
-    let message = '';
+// const putFines = async (req, res = response) => {
+//     const body = req.body;
+//     let message = '';
 
-    try {
-        const { idfines, state } = body;
-        const existingFines = await Fines.findOne({
-            where: { idfines: idfines },
-        });
+//     try {
+//         const { idfines, state } = body;
+//         const existingFines = await Fines.findOne({
+//             where: { idfines: idfines },
+//         });
 
-        if (existingFines) {
-            // Verificar si la multa actual tiene el campo paymentproof
-            if (existingFines.paymentproof == null) {
-                // Procesar y almacenar paymentproof si existe
-                console.log("Esto es lo que se envia"+req.files.paymentproof)
-                const imageUrl = await upload(req.files.paymentproof, ['pdf', 'jpg', 'jpeg', 'png'], 'File');
-                console.log("Esto se imprime nuevo"+imageUrl);
+//         if (existingFines) {
+//             // Verificar si la multa actual tiene el campo paymentproof
+//             if (existingFines.paymentproof == null) {
+//                 // Procesar y almacenar paymentproof si existe
+//                 console.log("Esto es lo que se envia"+req.files.paymentproof)
+//                 const imageUrl = await upload(req.files.paymentproof, ['pdf', 'jpg', 'jpeg', 'png'], 'File');
+//                 console.log("Esto se imprime nuevo"+imageUrl);
 
-                // Actualizar la multa con el nuevo estado y paymentproof
-                await Fines.update({
-                    state: state,
-                    paymentproof: imageUrl,
-                }, {
-                    where: { idfines: idfines },
-                });
+//                 // Actualizar la multa con el nuevo estado y paymentproof
+//                 await Fines.update({
+//                     state: state,
+//                     paymentproof: imageUrl,
+//                 }, {
+//                     where: { idfines: idfines },
+//                 });
 
-                message = 'Multa modificada exitosamente con nuevo paymentproof.';
-            } else {
+//                 message = 'Multa modificada exitosamente con nuevo paymentproof.';
+//             } else {
                 
-                console.log("Esto es lo que se envia reemplazo "+req.files.paymentproof)
-                const newfile = await updateFile(req.files.paymentproof, existingFines.paymentproof, ['pdf', 'jpg', 'jpeg', 'png'], 'File');
-                console.log( "Esto es lo que se actualizo reemplazo "+newfile);
-                const results = await Fines.update({
-                    state: state,
-                    paymentproof: newfile,
-                }, {
-                    where: { idfines: idfines },
-                });
+//                 console.log("Esto es lo que se envia reemplazo "+req.files.paymentproof)
+//                 const newfile = await updateFile(req.files.paymentproof, existingFines.paymentproof, ['pdf', 'jpg', 'jpeg', 'png'], 'File');
+//                 console.log( "Esto es lo que se actualizo reemplazo "+newfile);
+//                 const results = await Fines.update({
+//                     state: state,
+//                     paymentproof: newfile,
+//                 }, {
+//                     where: { idfines: idfines },
+//                 });
 
-                if (newfile != null || newfile != '') {
-                    message = 'Multa modificada exitosamente.',results;
-                } else {
-                    message = 'No se encontró una multa con ese ID', results;
-                }
-            }
-        } else {
-            message = 'No se encontró una multa con ese ID';
+//                 if (newfile != null || newfile != '') {
+//                     message = 'Multa modificada exitosamente.',results;
+//                 } else {
+//                     message = 'No se encontró una multa con ese ID', results;
+//                 }
+//             }
+//         } else {
+//             message = 'No se encontró una multa con ese ID';
+//         }
+//     } catch (error) {
+//         message = 'Error al modificar multa: ' + error.message;
+//     }
+
+//     res.json({
+//         fines: message,
+//     });
+// };
+
+const putFines = async (req, res = response) => {
+    try {
+        const { idfines, state} = req.body;
+
+        const fine = await Fines.findOne({ where: { idfines: idfines } });
+
+        if (!fine) {
+            return res.status(404).json({ message: 'No se encontró una multa con ese ID' });
         }
-    } catch (error) {
-        message = 'Error al modificar multa: ' + error.message;
-    }
 
-    res.json({
-        fines: message,
-    });
+        let results;
+
+        if (req.files.paymentproof) {
+            const newImg = fine.paymentproof == "" || fine.paymentproof == null ?
+                await upload(req.files.paymentproof, ['png', 'jpg', 'jpeg', 'pdf'], 'Images') :
+                await updateFile(req.files, fine.paymentproof, ['png', 'jpg', 'jpeg', 'pdf'], 'Images');
+
+            results = await fine.update({
+                state: state,
+                paymentproof: newImg,
+            }, { where: { idfines: idfines } });
+
+            res.json({
+                message: 'Multa modificada exitosamente con archivo.',
+                results,
+            });
+        } else {
+            results = await fine.update({
+                state: state,
+            }, { where: { idfines: idfines } });
+
+            res.json({
+                message: 'Multa modificada exitosamente sin archivo.',
+                results,
+            });
+        }
+
+    } catch (error) {
+        console.error('Error al modificar multa:', error);
+        res.status(500).json({
+            error: error.message,
+        });
+    }
 };
+
 
 
 
