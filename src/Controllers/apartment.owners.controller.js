f241c298f90017a26fb3b2daf2c60eaab6d6a6b6
Emmanuel tabares <emanuel_tabares@hotmail.com>
2024-02-03 23:15:47 -0500

    Add towers to getOneApartment response

--- src/Controllers/apartment.owners.controller.js
+++ src/Controllers/apartment.owners.controller.js
@@ -17,8 +17,7 @@ const getOneApartmentOwners = async (req, res = response) => {
 
         const owners = await OwnersModel.findAll();
 
-        const users = await UserModel
-        .findAll();
+        const users = await UserModel.findAll();
 
 
         const data = apartmentOwners.map(ao => {
@@ -61,19 +60,13 @@ const getAllApartmentOwners = async (req, res) => {
 
         const apartmentOwners = await ApartmentOwnerModel.findAll();
 
-        const apartments = await ApartmentModel.findAll({
-            attributes: ['idApartment', 'apartmentName', 'area', 'status']
+        const apartments = await ApartmentModel.findAll();
 
-        });
-
-        const owners = await OwnersModel.findAll({
-            attributes: ['idOwner', 'docType', 'docNumber', 'name', 'lastName', 'email', 'phoneNumber', 'status'],
-
-        });
+        const owners = await OwnersModel.findAll();
 
         const data = apartmentOwners.map(ao => {
 
-            const apartment = apartments.find(apartment => apartment.idSpace === ao.idApartment);
+            const apartment = apartments.find(apartment => apartment.idApartment === ao.idApartment);
             const owner = owners.find(ow => ow.idOwner === ao.idOwner);
             
             return {
