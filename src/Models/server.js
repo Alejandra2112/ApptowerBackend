77efddff2986ec5083345c3e214f3ef0f1f2226d
Alejandra2112 <alejandramejia1256@gmail.com>
2024-02-13 14:31:21 -0500

    changes in server

--- src/Models/server.js
+++ src/Models/server.js
@@ -79,16 +79,13 @@ class Servers {
     this.app.use(cookieParser());
 
     this.app.use((req, res, next) => {
-      const allowedOrigins = ['http://localhost:3000', 'http://localhost:5173', 'https://apptowerbackend.onrender.com'];
+
       const origin = req.headers.origin;
-
-      if (allowedOrigins.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-      }
-
+      res.setHeader('Access-Control-Allow-Origin', origin || '*');
       res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
       res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
       res.setHeader('Access-Control-Allow-Credentials', 'true');
+
 
       if (req.method === 'OPTIONS') {
         return res.status(200).end();
