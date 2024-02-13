9c18ba7211195db4f930a3a81d5915e85e17019b
Alejandra2112 <alejandramejia1256@gmail.com>
2024-02-12 19:09:46 -0500

    changes

--- src/Models/server.js
+++ src/Models/server.js
@@ -84,7 +84,7 @@ class Servers {
       if (origin === allowedOrigin) {
         res.setHeader('Access-Control-Allow-Origin', origin);
       } else {
-        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
       }
 
       res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
