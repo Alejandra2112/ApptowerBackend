f94fd9a35613ca1348005bf7887876ac1fad0f99
Emmanuel tabares <emanuel_tabares@hotmail.com>
2024-03-03 20:07:07 -0500

    Add middlewares to spaces proccess

---
+++ src/Middlewares/apartment.residents.middleware.js
@@ -0,0 +1,238 @@
+const { check } = require('express-validator');
+const SpacesModel = require('../Models/spaces.model');
+const ResidentModel = require('../Models/resident.model');
+const ApartmentResidentModel = require('../Models/apartment.residents.model');
+const ApartmentModel = require('../Models/apartment.model');
+
+
+const apartmentResidentValidationForPost = [
+
+    check('idApartment')
+        .notEmpty().withMessage('El apartamento es obligatorio.')
+        .isInt().withMessage('El ID de la apartamento debe ser un número entero positivo.')
+        .custom(async (value) => {
+
+            const existingApartment = await ApartmentModel.findOne({ where: { idApartment: value } });
+
+            if (existingApartment) {
+                return true
+            }
+            else throw new Error('El apartamento selecionado no esta en el sistema.');
+
+        }),
+
+
+    check('idResident')
+        .notEmpty().withMessage('El residente es obligatorio.')
+        .isInt().withMessage('El ID del residente debe ser un número entero positivo.')
+        .custom(async (value) => {
+
+            const existingResident = await ResidentModel.findOne({ where: { idResident: value } });
+
+            if (existingResident) {
+                return true
+            }
+            else throw new Error('El residente selecionado no esta en el sistema.');
+
+        })
+
+        // Validation to prevent a resident from being registered more than once in the same apartment
+
+        .custom(async (value, { req }) => {
+
+            const body = req.body;
+
+            const existingRecord = await ApartmentResidentModel.findOne({
+                where: { idResident: value, idApartment: body.idApartment }
+            });
+
+            if (existingRecord) {
+                throw new Error('Este residente ya fue asignado a este apartamento.');
+            } else {
+                return true;
+            }
+        })
+
+        // Validation to ensure that a resident is not assigned to another apartment
+
+        .custom(async (value, { req }) => {
+
+            const body = req.body;
+
+            const existingRecord = await ApartmentResidentModel.findAll({
+                where: { idResident: value }
+            });
+
+            console.log(existingRecord, 'New validation')
+
+
+            if (existingRecord && existingRecord.length >= 1 && existingRecord[0].status == 'Active') {
+
+                throw new Error('Este residente ya fue asignado a otro apartamento.');
+
+            } else {
+
+                return true;
+
+            }
+        }),
+
+
+
+    check('residentStartDate')
+        .notEmpty().withMessage('La fecha de inicio es obligatorio.')
+        .custom((value) => {
+            const residentStartDate = new Date(value);
+            const today = new Date();
+
+            if (residentStartDate > today) {
+                throw new Error('La fecha de inicicio no puede ser superior a hoy.');
+            }
+            return true;
+        }),
+
+
+
+]
+
+const apartmentResidentValidationForPut = [
+
+    check('idApartment')
+        .notEmpty().withMessage('El apartamento es obligatorio.')
+        .isInt().withMessage('El ID de la apartamento debe ser un número entero positivo.')
+        .custom(async (value) => {
+
+            const existingApartment = await ApartmentModel.findOne({ where: { idApartment: value } });
+
+            if (existingApartment) {
+                return true
+            }
+            else throw new Error('El apartamento selecionado no esta en el sistema.');
+
+        }),
+
+
+    check('idResident')
+        .optional()
+        .isInt().withMessage('El ID del residente debe ser un número entero positivo.')
+        .custom(async (value) => {
+
+            const existingResident = await ResidentModel.findOne({ where: { idResident: value } });
+
+            if (existingResident) {
+                return true
+            }
+            else throw new Error('El residente selecionado no esta en el sistema.');
+
+        })
+
+    // // Validation to prevent a resident from being registered more than once in the same apartment
+
+    // .custom(async (value, { req }) => {
+
+    //     const body = req.body;
+
+    //     const existingRecord = await ApartmentResidentModel.findOne({
+    //         where: { idResident: value, idApartment: body.idApartment }
+    //     });
+
+    //     if (existingRecord) {
+    //         throw new Error('Este residente ya fue asignado a este apartamento apartamento.');
+    //     } else {
+    //         return true;
+    //     }
+    // })
+
+    // Validation to ensure that a resident is not assigned to another apartment
+
+    // .custom(async (value, { req }) => {
+
+    //     const body = req.body;
+
+    //     const existingRecord = await ApartmentResidentModel.findAll({
+    //         where: { idResident: value }
+    //     });
+
+    //     console.log(existingRecord, 'New validation')
+
+
+    //     if (existingRecord && existingRecord.length >= 1) {
+
+    //         throw new Error('Este residente ya fue asignado a otro apartamento.');
+
+    //     } else {
+
+    //         return true;
+
+    //     }
+    // })
+    ,
+
+
+
+
+    check('residentStartDate')
+        .notEmpty().withMessage('La fecha de inicio es obligatorio.')
+        .custom((value) => {
+            const residentStartDate = new Date(value);
+            const today = new Date();
+
+            if (residentStartDate > today) {
+                throw new Error('La fecha de inicicio no puede ser superior a hoy.');
+            }
+            return true;
+        }),
+
+    check('residentEndDate')
+        .notEmpty().withMessage('La fecha de fin es obligatorio.')
+        .custom((value, { req }) => {
+            const residentEndDate = new Date(value);
+            const residentStartDate = new Date(req.body.residentStartDate);
+
+            console.log(residentEndDate >= residentStartDate, residentStartDate, residentEndDate)
+            if (residentEndDate <= residentStartDate) {
+                throw new Error('La fecha de fin debe ser mayor a la fecha de inicio.');
+            }
+            return true;
+
+        }),
+    check('status')
+        .isIn(['Active', 'Inactive'])
+        .withMessage('El estado del propietario no es válido.')
+        .custom((value, { req }) => {
+            const status = req.body.status;
+
+            console.log(status, 'estado')
+            if (status === 'Active') throw new Error('Debe estar inactivo paramarcar la fecha de fin.')
+
+            return true;
+
+        }),
+
+]
+
+const apartmentResidentValidationForDelete = [
+
+    check('idApartmentResident')
+        .notEmpty().withMessage('El ID es obligatorio.')
+        .isInt().withMessage('El ID debe ser un número entero positivo.')
+    // .custom(async (value) => {
+
+    //     const apartmentResident = await ApartmentResidentModel.findOne({ idApartmentResident: value })
+
+    //     console.log(apartmentResident, 'apartmentResident')
+    //     if (apartmentResident && apartmentResident.status == 'Active') {
+    //         throw new Error('No se puede realizar esta acción porque el residente está actualmente activo en el apartamento.')
+    //     }
+
+    //     return true
+    // })
+]
+
+module.exports = {
+
+    apartmentResidentValidationForPost,
+    apartmentResidentValidationForPut,
+    apartmentResidentValidationForDelete
+
+}
--- src/Middlewares/apartments.middleware.js
+++ src/Middlewares/apartments.middleware.js
@@ -3,7 +3,56 @@ const { check } = require('express-validator');
 const TowerModel = require('../Models/tower.model');
 const ApartmentModel = require('../Models/apartment.model');
 
-const apartmentValidationForPost = [
+const idApartmentValidationsForPost = [
+
+    check('idApartment')
+        .notEmpty().withMessage('El ID del apartamento es obligatorio.')
+        .isInt().withMessage('El ID del apartamento es obligatorio.')
+        .custom(async (value) => {
+
+            const existingApartment = await ApartmentModel.findOne({ where: { idApartment: value } });
+
+            if (existingApartment) {
+                return true
+            }
+            else throw new Error('El apartamento selecionado no esta en el sistema.');
+
+        }),
+]
+
+const apartmentsVlidationForPost = [
+
+
+    check('apartmentsFloor')
+        .notEmpty().withMessage('El número de pisos de los apartamentos es obligatorio.')
+        .isInt({ min: 1 }).withMessage('El número de pisos de los apartamentos debe ser un número entero positivo.'),
+
+    check('idTower')
+        .notEmpty().withMessage('El ID de la torre es obligatorio.')
+        .isInt({ min: 1 }).withMessage('El ID de la torre debe ser un número entero positivo.'),
+
+    check('rangeStart')
+        .notEmpty().withMessage('El rango de inicio es obligatorio.')
+        .isInt({ min: 1 }).withMessage('El rango de inicio debe ser un número entero positivo.'),
+
+    check('rangeEnd')
+        .notEmpty().withMessage('El rango de fin es obligatorio.')
+        .isInt({ min: 1 }).withMessage('El rango de fin debe ser un número entero positivo.')
+        .custom((value, { req }) => {
+            if (parseInt(value) <= parseInt(req.body.rangeStart)) {
+                throw new Error('El rango de fin debe ser mayor que el rango de inicio.');
+            }
+            return true;
+        }),
+
+    check('area')
+        .notEmpty().withMessage('El área es obligatoria.')
+        .isFloat({ min: 0 }).withMessage('El área debe ser un número positivo.')
+];
+
+
+
+const apartmentValidationForPut = [
 
     // check('idApartment')
     //     .notEmpty().withMessage('El ID del apartamento es obligatorio.')
@@ -26,18 +75,26 @@ const apartmentValidationForPost = [
         .isLength({ max: 20 }).withMessage('El nombre del apartamento no puede exceder los 20 caracteres.')
         .matches(/^[a-zA-Z0-9]+$/)
         .withMessage('Nombre del apartamento solo debe tener numeros y letras.')
-        .custom(async (value) => {
-            const apartment = await ApartmentModel.findOne({ where: { apartmentName: value } });
-            if (apartment) {
+        .custom(async (value, { req }) => {
+            const body = req.body
+
+            const existingApartmentById = await ApartmentModel.findOne({ where: { idApartment: body.idApartment } });
+            const existingApartmentByName = await ApartmentModel.findOne({ where: { apartmentName: value } });
+
+            if (!existingApartmentById || !existingApartmentByName) {
+                return true;
+            } else if (existingApartmentById.apartmentName !== existingApartmentByName.apartmentName) {
                 throw new Error(`Nombre del apartamento "${value}" ya está en uso.`);
+            } else {
+                return true;
             }
-            return true;
+
         }),
 
     check('area')
         .notEmpty().withMessage('El área del apartamento es obligatoria.')
         .isNumeric().withMessage('El área del apartamento debe ser un número.')
-        .isFloat({ min: 0 }).withMessage('El área del apartamento debe ser un número mayor o igual a cero.')
+        .isFloat({ min: 1 }).withMessage('El área del apartamento debe ser un número mayor a cero.')
 
 
 
@@ -59,6 +116,8 @@ const apartmentValidationForPost = [
 
 module.exports = {
 
-    apartmentValidationForPost,
+    idApartmentValidationsForPost,
+    apartmentsVlidationForPost,
+    apartmentValidationForPut,
 
 }
