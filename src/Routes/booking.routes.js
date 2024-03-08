646df5f504b706d75b9b59fe86afaae930e45291
Emmanuel tabares <emanuel_tabares@hotmail.com>
2024-03-08 18:18:23 -0500

    Update booking controller

--- src/Routes/booking.routes.js
+++ src/Routes/booking.routes.js
@@ -1,10 +1,11 @@
 const {Router} = require('express');
 const route = Router();
-const {getBooking, postBooking, putBooking, getOneBookingbySpaces} = require('../Controllers/booking.controller');
+const {getBooking, postBooking, putBooking, getOneBookingbySpaces, getOneBooking} = require('../Controllers/booking.controller');
 
 route.get('/', getBooking);
 route.post('/', postBooking);
 route.put('/', putBooking);
 route.get('/:idSpace', getOneBookingbySpaces);
+route.get('/one/:idbooking', getOneBooking);
 
 module.exports = route;
