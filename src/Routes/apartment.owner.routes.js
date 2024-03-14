fb5330fb332fe7f90699f6c940ed459adcd1cd23
Emmanuel tabares <emanuel_tabares@hotmail.com>
2024-03-14 13:31:35 -0500

    Update and add middlewares to apartment residents and apartment owners

--- src/Routes/apartment.owner.routes.js
+++ src/Routes/apartment.owner.routes.js
@@ -1,16 +1,16 @@
 const { Router } = require('express')
 const route = Router()
 const { getOneApartmentOwners, getAllApartmentOwners, postApartmentOwner, putApartmentOwner, deleteApartmentOwner } = require('../Controllers/apartment.owners.controller')
-const { apartmentOwnerValidationForPut, apartmentOwnerValidationForPost, OwnershipStartDateValidationForPost } = require('../Middlewares/apartment.owners.middleware');
+const { apartmentOwnerValidationForPut, apartmentOwnerValidationForPost, OwnershipStartDateValidationForPost, apartmentOwnerValidationForDelete } = require('../Middlewares/apartment.owners.middleware');
 const { idApartmentValidationsForPost } = require('../Middlewares/apartments.middleware');
-const validator = require('../Middlewares/validation.middleware');
 const { createAapartmentOwnerValidationforPost } = require('../Middlewares/owner.middleware');
+const validator = require('../Middlewares/validation.middleware');
 
 route.get('/:idApartment', getOneApartmentOwners)
 route.get('/', getAllApartmentOwners)
 route.post('/', idApartmentValidationsForPost, createAapartmentOwnerValidationforPost,
     apartmentOwnerValidationForPost, OwnershipStartDateValidationForPost, validator, postApartmentOwner)
 route.put('/', idApartmentValidationsForPost, apartmentOwnerValidationForPut, validator, putApartmentOwner)
-route.delete('/', deleteApartmentOwner)
+route.delete('/', apartmentOwnerValidationForDelete, validator, deleteApartmentOwner)
 
 module.exports = route  
