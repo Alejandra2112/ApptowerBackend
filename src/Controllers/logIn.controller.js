8d3349291939687a612e60b65dc597cee5f33004
Alejandra2112 <alejandramejia1256@gmail.com>
2024-03-23 20:59:40 -0500

    changes

--- src/Controllers/logIn.controller.js
+++ src/Controllers/logIn.controller.js
@@ -85,8 +85,6 @@ const logIn = async (req, res) => {
     res.json({
       message: 'Inicio de sesión exitoso',
       token,
-      user,
-      PermissionsAndPrivileges
     });
   } catch (error) {
     console.error('Error al iniciar sesión:', error);
