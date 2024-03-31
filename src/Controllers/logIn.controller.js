b2603c27637275ae81eae9a84a7dbb1d41adbf4d
Alejandra2112 <alejandramejia1256@gmail.com>
2024-03-30 21:04:16 -0500

    changes in login

--- src/Controllers/logIn.controller.js
+++ src/Controllers/logIn.controller.js
@@ -84,7 +84,8 @@ const logIn = async (req, res) => {
     res.json({
       message: 'Inicio de sesión exitoso',
       user: encodeURIComponent(JSON.stringify(userPayload)),
-      PermissionsAndPrivileges: encodeURIComponent(JSON.stringify(RolePrivilegesPayload))
+      PermissionsAndPrivileges: encodeURIComponent(JSON.stringify(RolePrivilegesPayload)),
+      token
     });
   } catch (error) {
     console.error('Error al iniciar sesión:', error);
