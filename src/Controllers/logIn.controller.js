602503456b4fe9fa6961c06240ea859b1cc19379
Alejandra2112 <alejandramejia1256@gmail.com>
2024-03-31 17:25:01 -0500

    changes in login

--- src/Controllers/logIn.controller.js
+++ src/Controllers/logIn.controller.js
@@ -77,13 +77,13 @@ const logIn = async (req, res) => {
       expiresIn: '365d',
     });
 
-    res.cookie('token', token, { httpOnly: false, secure: true, sameSite: 'none' });
-    res.cookie('user', JSON.stringify(userPayload), { httpOnly: false, secure: true, sameSite: 'none' });
-    res.cookie('permisosAndPrivileges', JSON.stringify(RolePrivilegesPayload), { httpOnly: false, secure: true, sameSite: 'none' });
-
     res.json({
       message: 'Inicio de sesión exitoso',
+      token: token,
+      user: userPayload,
+      permisosAndPrivileges: RolePrivilegesPayload,
     });
+
   } catch (error) {
     console.error('Error al iniciar sesión:', error);
     res.status(500).json({ message: 'Error' });
